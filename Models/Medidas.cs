5d58b1f78ee6d36d5ead3025ae455979bb449d22
Marcelo Pereira Santos <lemarcelo@outlook.com>
2021-07-06 21:32:36 -0300

    Add project files.

---
+++ Models/Medidas.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace testeef.Models
+{
+    public class Medidas
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage ="Limite 20 caracteres")]
+        [MinLength(3, ErrorMessage ="Tamanho minimo 3 caracteres")]
+        public string Veiculo { get; set; }
+        
+        [MaxLength(4, ErrorMessage ="Limite 7 caracteres")]
+        [MinLength(3, ErrorMessage ="Tamanho minimo 3 caracteres")]
+        public string Pistao { get; set; }
+        
+        [MaxLength(20, ErrorMessage ="Limite 7 caracteres")]
+        [MinLength(3, ErrorMessage ="Tamanho minimo 3 caracteres")]
+        public string Pino { get; set; }
+        
+        [MaxLength(20, ErrorMessage ="Limite 7 caracteres")]
+        [MinLength(3, ErrorMessage ="Tamanho minimo 3 caracteres")]
+        public string Curso { get; set; }
+        
+
+    }
+}
