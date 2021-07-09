a4f7fa5ee73b2287d7916ee2f91f819699cfd290
Marcelo Pereira Santos <lemarcelo@outlook.com>
2021-07-08 23:42:22 -0300

    correções

--- Models/Medidas.cs
+++ Models/Medidas.cs
@@ -13,15 +13,15 @@ namespace testeef.Models
         public string Veiculo { get; set; }
         
         [MaxLength(4, ErrorMessage ="Limite 7 caracteres")]
-        [MinLength(3, ErrorMessage ="Tamanho minimo 3 caracteres")]
+        [Range(3, int.MaxValue, ErrorMessage ="Tamanho minimo 3 caracteres")]
         public string Pistao { get; set; }
         
         [MaxLength(20, ErrorMessage ="Limite 7 caracteres")]
-        [MinLength(3, ErrorMessage ="Tamanho minimo 3 caracteres")]
+        [Range(3, int.MaxValue, ErrorMessage ="Tamanho minimo 3 caracteres")]
         public string Pino { get; set; }
         
         [MaxLength(20, ErrorMessage ="Limite 7 caracteres")]
-        [MinLength(3, ErrorMessage ="Tamanho minimo 3 caracteres")]
+        [Range(3, int.MaxValue, ErrorMessage ="Tamanho minimo 3 caracteres")]
         public string Curso { get; set; }
         
 
